fc8939c8f38528f4127209e8658e7bcc37f9d34b
kstrong <kevin@wordsauce.org>
2013-12-14 23:51:47 -0800

    setting up site on slim+twig, converting products page to use a layout template

---
+++ public/index.php
@@ -0,0 +1,34 @@
+<?php
+require __DIR__.'/../vendor/autoload.php';
+
+$app = new \Slim\Slim(array(
+  'templates.path' => '../templates',
+  'view' => new \Slim\Views\Twig()
+));
+
+$view = $app->view();
+$view->parserOptions = array(
+  'debug' => true,
+  'cache' => dirname(__FILE__).'/../cache'
+);
+$view->parserExtensions = array(
+  new \Slim\Views\TwigExtension()
+);
+
+$app->get('/', function () use ($app) {
+  $app->render('homepage.html');
+});
+
+$app->get('/products', function () use ($app) {
+  $app->render('products.html');
+});
+
+$app->get('/foo', function () {
+  echo "FOOS";
+});
+
+$app->get('/test', function () {
+  echo "TEST";
+});
+
+$app->run();
