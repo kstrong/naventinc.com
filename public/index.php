20f63a5fe6abe50a0d8d74559b38131ceba41ab9
kstrong <kevin@wordsauce.org>
2014-01-04 10:54:52 -0800

    update layout to link to projects, fantasy dream projects, shorthand for specifying projects/services routes

--- public/index.php
+++ public/index.php
@@ -31,29 +31,44 @@ $app->get('/projects/neg-client-projects', function () use ($app) {
   $app->render('pages/client_projects.html');
 });
 
+$app->get('/projects/projects-in-development', function () use ($app) {
+  echo 'not implemented';
+});
+
+$projects_pages = [
+  // client projects
+  'fantasy-dream' => 'fantasy_dream.html',
+  'film-tv-production' => 'film_tv_production.html',
+  'guinness-museum' => 'guinness_museum.html',
+  'hollywood-wax-museum' => 'hollywood_wax_museum.html',
+  'misc-projects' => 'misc_projects.html',
+  'monster-mansion' => 'monster_mansion.html',
+  // projects in development
+];
+
+foreach ($projects_pages as $uri => $template) {
+  $app->get('/projects/'.$uri, function () use ($app, $template) {
+    $app->render('pages/projects/'.$template, ['slideshow' => true]);
+  });
+}
+
 $app->get('/services', function () use ($app) {
   $app->render('services.html');
 });
 
-$app->get('/services/robotics-engineering', function () use ($app) {
-  $app->render('robotics_engineering.html', ['slideshow' => true]);
-});
+$services_pages = [
+  'robotics-engineering' => 'robotics_engineering.html',
+  'special-effects' => 'special_effects.html',
+  'show-action-equipment' => 'show_action_equipment.html',
+  'wax-figures' => 'wax_figures.html',
+  'maintains-rehab' => 'maintains_rehab.html',
+];
 
-$app->get('/services/special-effects', function () use ($app) {
-  $app->render('special_effects.html', ['slideshow' => true]);
-});
-
-$app->get('/services/show-action-equipment', function () use ($app) {
-  $app->render('show_action_equipment.html', ['slideshow' => true]);
-});
-
-$app->get('/services/wax-figures', function () use ($app) {
-  $app->render('wax_figures.html', ['slideshow' => true]);
-});
-
-$app->get('/services/maintains-rehab', function () use ($app) {
-  $app->render('maintains_rehab.html', ['slideshow' => true]);
-});
+foreach ($services_pages as $uri => $template) {
+  $app->get('/services/'.$uri, function () use ($app, $template) {
+    $app->render($template, ['slideshow' => true]);
+  });
+}
 
 $app->get('/company', function () use ($app) {
   $app->render('company.html');
